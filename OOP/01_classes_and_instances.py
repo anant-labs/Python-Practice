883f5aa6e34e653a720801f663c21843c047ed65
Anant <anantmathur1070@gmail.com>
2026-07-11 01:28:33 +0530

    Add classes and instances program

---
+++ OOP/01_classes_and_instances.py
@@ -0,0 +1,23 @@
+class Employee:
+    def __init__ (self,first,last,pay) :
+        self.first = first
+        self.last = last
+        self.pay = pay 
+        self.email = first + '.' + last + '@company.com'
+    def fullname(self) : 
+        return '{} {}' .format(self.first,self.last)
+
+emp_1 = Employee('Anant','Mathur','90000')
+emp_2 = Employee('Rohit','Gupta',"50000")
+emp_3 = Employee('Keshav','Rao','75000')
+
+print(emp_1.fullname())
+print(emp_2.fullname())
+print(emp_3.fullname())
+
+print(emp_1.email)
+print(emp_2.email)
+print(emp_3.email)
+
+
+
